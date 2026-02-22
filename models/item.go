eec5c978b507f6aef84a61373e35886ae97a3ec9
Fajar Satria <fajar.satria@innomanic.com>
2026-02-22 15:16:57 +0700

    feat: Add Price and Stock fields to the Item model and update Go module dependencies.

--- models/item.go
+++ models/item.go
@@ -3,7 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type Item struct {
-	gorm.Model         // adds ID, CreatedAt, UpdatedAt, DeletedAt
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	gorm.Model          // adds ID, CreatedAt, UpdatedAt, DeletedAt
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Stock       int     `json:"stock"`
 }
